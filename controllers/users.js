a2bb3615cc4f3d0217349fe05f494a727dff84f1
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-11-02 07:20:21 +0300

    refact: настройка cors

--- controllers/users.js
+++ controllers/users.js
@@ -70,7 +70,6 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: 'None',
-          secure: true,
         })
         .send({ message: successMessages.loginMessage });
     })
@@ -83,7 +82,6 @@ module.exports.logout = (req, res, next) => {
     res.clearCookie('jwt', {
       httpOnly: true,
       sameSite: 'None',
-      secure: true,
     });
     res.send({ message: successMessages.logoutMessage });
   } catch (err) {
